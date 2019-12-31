da985a99e5d362be67ddb690a3b56f972206b728
sunzhengyu <sunzhengyu@tuzhanai.com>
2019-12-31 19:14:02 +0800

    dev: 限流demo  ratelimiter

---
+++ web/ratelimiter-demo/src/main/java/com/steamedfish/springboot/ratelimiterdemo/RatelimiterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.steamedfish.springboot.ratelimiterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelimiterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatelimiterDemoApplication.class, args);
+    }
+
+}
---
+++ web/ratelimiter-demo/src/test/java/com/steamedfish/springboot/ratelimiterdemo/RatelimiterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steamedfish.springboot.ratelimiterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
